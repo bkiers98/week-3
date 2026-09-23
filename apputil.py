047ecec2e2a5cddaa9eb55522c3394c21a4a396d
bkiers98 <167789286+bkiers98@users.noreply.github.com>
2026-09-22 22:55:58 -0400

    Debug for autograder

--- apputil.py
+++ apputil.py
@@ -58,8 +58,12 @@ def task_2():
     the amount of admissions for each year in the dataset.
     '''
     df_bellevue['year'] = pd.to_datetime(df_bellevue['date_in']).dt.year
-    df_year_admins = df_bellevue['year'].value_counts().reset_index(name='total_admissions')
-    return df_year_admins
+    df_year_admins = df_bellevue.groupby('year') \
+                                .size() \
+                                .reset_index(name='total_admissions') \
+                                .sort_values('year') \
+                                .reset_index(drop=True)
+    return df_year_admins[['year', 'total_admissions']]
 
 
 def task_3():
