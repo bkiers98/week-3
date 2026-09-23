0fc4d22219cb9c9a581bd6ebfc78ea06dbb74d6e
bkiers98 <167789286+bkiers98@users.noreply.github.com>
2026-09-22 21:26:04 -0400

    Add remaining exercises

--- apputil.py
+++ apputil.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 
 # update/add code below ...
@@ -20,9 +21,9 @@ def to_binary(i):
     Given an integer, this function will return its binary representation.
     '''
     if i == 0:
-        return 0
+        return '0'
     elif i == 1:
-        return 1
+        return '1'
     else:
         return str(to_binary(i // 2)) + str(i%2)
 
@@ -32,6 +33,10 @@ url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/
 df_bellevue = pd.read_csv(url)
 
 def task_1():
+    '''
+    Returns a list of all column names, sorted in order from least missing
+    values to most missing values.
+    '''
     bellevue_cols = []
 
     #replace '?' gender with NaN
@@ -43,4 +48,33 @@ def task_1():
     bellevue_cols.sort(key=lambda x: x[1])
     bellevue_cols_sorted = [col[0] for col in bellevue_cols]
 
-    return bellevue_cols_sorted    
+    return bellevue_cols_sorted
+
+
+def task_2():
+    '''
+    Returns a data frame with two columns ('year' and 'total_admissions'), describing
+    the amount of admissions for each year in the dataset.
+    '''
+    df_bellevue['year'] = pd.to_datetime(df_bellevue['date_in']).dt.year
+    df_year_admins = df_bellevue['year'].value_counts().reset_index(name='total_admissions')
+    return df_year_admins
+
+
+def task_3():
+    '''
+    Returns a series with 'gender' as the index, and the average age for each gender
+    as the value.
+    '''
+    df_gender_mean_age = df_bellevue.groupby('gender') \
+                                    [['age']].mean()
+    return df_gender_mean_age
+
+
+def task_4():
+    '''
+    Returns a list of the 5 most common professions in order of prevalence. 
+    '''
+    professions = df_bellevue['profession'].value_counts().sort_values(ascending=False).index[:5].to_list()
+    
+    return professions
